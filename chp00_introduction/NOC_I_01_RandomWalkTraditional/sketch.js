ddb65b4bff2851b22e9d704ecdd57448cd116495
anugrahandi <anugrahandi@gmail.com>
2019-12-22 09:42:23 +0700

    coba tambah introduction contoh walk traditionl

--- chp00_introduction/NOC_I_01_RandomWalkTraditional/sketch.js
+++ chp00_introduction/NOC_I_01_RandomWalkTraditional/sketch.js
@@ -13,18 +13,25 @@ function setup() {
 function draw() {
   walker.step();
   walker.render();
+  fill(94, 94, 71);
+  rect(5, 10, 100, 30);
+  textSize(20);
+  fill(226, 230, 21);
+  text(walker.step_number, 10, 30);
 }
 
 class Walker {
   constructor(){
     this.x = width/2;
     this.y = height/2;
+    this.step_number = 0;
   }
 
   render() {
     stroke(0);
     // point(this.x,this.y);
     circle(this.x,this.y,5);
+    this.step_number++;
   }
 
   step() {

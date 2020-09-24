a9758e09e770f17a40c29c5694d5360887763069
anugrahandi <anugrahandi@gmail.com>
2020-09-24 17:23:22 +0700

    before upstream fetch

--- chp00_introduction/NOC_I_01_C_RandomWalkTraditional-8Ways - withGUI - Uploaded to anugrahandi/1_helloworld/sketch.js
+++ chp00_introduction/NOC_I_01_C_RandomWalkTraditional-8Ways - withGUI - Uploaded to anugrahandi/1_helloworld/sketch.js
@@ -14,9 +14,13 @@ var arrWalker = [];
 function setup() {
   var canvas = createCanvas(640,480);
   canvas.parent('holder');
-  walker = new Walker();
-  walker_2 = new Walker();
-  walker_3 = new Walker();
+  for(let i = 0; i <= 100; i++) {
+    arrWalker.push(new Walker());
+    // arrWalker[i] = new Walker();
+  };
+  // walker = new Walker();
+  // walker_2 = new Walker();
+  // walker_3 = new Walker();
   // background(127);
   // background(myColor);
 
@@ -31,28 +35,45 @@ function setup() {
 
 function draw() {
   background(myColor);
-  walker.step();
-  walker.render();
-  walker_2.step();
-  walker_2.render();
-  walker_3.step();
-  walker_3.render();
+  for(let i = 0; i <= 100; i++) {
+    arrWalker[i].step();
+    arrWalker[i].render();
+  };
+  // walker.step();
+  // walker.render();
+  // walker_2.step();
+  // walker_2.render();
+  // walker_3.step();
+  // walker_3.render();
 }
 
 class Walker {
   constructor(){
     this.x = width/2;
     this.y = height/2;
+    this.move_arr = [0,0,0,0,0,0,0,0];
+    this.move_str = '';
   }
-
+  
   render() {
     stroke(0);
     // point(this.x,this.y);
     fill(255);
+    ellipseMode(CENTER);
     circle(this.x,this.y,Size);
+
+    this.print_array();
+
     fill(0);
     textSize(10);
-    text(this.choice,this.x,this.y+20);
+    textAlign(CENTER);
+    // text(this.choice,this.x,this.y+20);
+    text(this.move_str,this.x,this.y+25);
+  }
+  
+  print_array() {
+    this.move_arr[this.choice] = this.move_arr[this.choice] + 1;
+    this.move_str = this.move_arr.join(",");
   }
 
   step() {
